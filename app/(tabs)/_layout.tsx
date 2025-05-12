c952eedc13eddc2da8e3e1217588f408aeea0dc6
João Vitor Costa de Moura Santos <joaovitorcms1@gmail.com>
2025-05-12 16:44:07 -0300

    Add files via upload

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
+
+
+
+export default function TabLayout() {
+
+
+  return (
+    <Tabs>
+      <Tabs.Screen name='index' options={{headerShown: false, tabBarIcon: () => (<Text>📛</Text>)}}></Tabs.Screen>
+      <Tabs.Screen name='explore' options={{headerShown: false, tabBarIcon: () => (<Text>☢</Text>)}}></Tabs.Screen>
+      <Tabs.Screen name='list' options={{headerShown: false, tabBarIcon: () => (<Text>☢</Text>)}}></Tabs.Screen>
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+export default function tabThreeScreen() {
+    return (
+     <>
+     <Text>Teceira Pagina</Text>
+     </>
+    );
+  }
